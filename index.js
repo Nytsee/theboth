a9d5c642cc261331e2dd071a86c4d7b4ffcdc8a2
nytsee <jamelnaitsi@gmail.com>
2017-11-02 23:09:46 +0000

    playback test

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.post('/webhook/', function(req, res){
 
     if (event.postback) {
 			let text = JSON.stringify(event.postback)
-			sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
+			sendText(sender, "Postback received: "+text.substring(0, 200), token)
 			continue
 	}
 
